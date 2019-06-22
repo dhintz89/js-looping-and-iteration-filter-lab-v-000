8268db836c71fe6ba883cb7d000ac69189ad1ecf
Dan Hintz <dhintz@live.com>
2019-06-22 21:01:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file
 function findMatching(arr, string) {
-  const filteredDrivers = arr.filter(function (element) {return element.toLowerCase() === string.toLowerCase()})
+  const filteredDrivers = arr.filter(function (element) {return element.toLowerCase() === string.toLowerCase()});
+  return filteredDrivers;
 }
 
 function fuzzyMatch() {
